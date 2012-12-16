1695f8e4fa6b5de8669c6640c5d21c00a33562d2
Niklas <ryxali@gmail.com>
2012-12-17 00:14:28 +0100

    Added the default option names as final variables.

--- src/priceofprogress/OptionsFile.java
+++ src/priceofprogress/OptionsFile.java
@@ -13,9 +13,19 @@ import java.util.Scanner;
 public class OptionsFile {
 
 	private static OptionsFile optionsFile;
-	public ArrayList<?super Object> data;
+	public ArrayList<? super Object> data;
 
 	private static String path = "options.txt";
+	public static final String FIELD_RESOLUTION_X = "SizeX:";
+	public static final String FIELD_RESOLUTION_Y = "SizeY:";
+	public static final String FIELD_FULLSCREEN = "FullScreen:";
+	public static final String FIELD_KEY_MOVELEFT = "MvLeft:";
+	public static final String FIELD_KEY_MOVERIGHT = "MvRight:";
+	public static final String FIELD_KEY_JUMP = "Jump:";
+	public static final String FIELD_KEY_FIRE = "Fire:";
+	public static final String FIELD_KEY_INTERRACT = "Interact:";
+	public static final String FIELD_VOLUME_MUSIC = "MusicVolume:";
+	public static final String FIELD_VOLUME_SOUND = "SoundVolume:";
 
 	public static OptionsFile get() {
 		if (optionsFile == null) {
@@ -24,86 +34,49 @@ public class OptionsFile {
 		return optionsFile;
 	}
 
-	public ArrayList<?super Object> getData() {
+	public ArrayList<? super Object> getData() {
 		return data;
 	}
 
 	private OptionsFile() {
 		data = loadData();
 	}
-	
+
 	public Integer fetchIntegerFromOptions(String name) {
 		if (data.contains(name)) {
 			return (Integer) data.get(data.indexOf(name) + 1);
-		}else{
+		} else {
 			return null;
 		}
 	}
+
 	public String fetchStringFromOptions(String name) {
 		if (data.contains(name)) {
 			return (String) data.get(data.indexOf(name) + 1);
-		}else{
+		} else {
 			return null;
 		}
 	}
+
+	/**
+	 * returns a boolean value matching the option name
+	 * 
+	 * @param name
+	 * @return
+	 */
 	public Boolean fetchBooleanFromOptions(String name) {
 		if (data.contains(name)) {
 			return (Boolean) data.get(data.indexOf(name) + 1);
-		}else{
+		} else {
 			return null;
 		}
 	}
-	/**
-	 * 
-	 * @param name
-	 *            the option name
-	 * @return an integer matching <b>name</b>
-	 */
-	/*public int fetchIntegerFromOptions(String name) throws Exception {
-		if (data.contains(name)) {
-			return Integer.valueOf(data.get(data.indexOf(name) + 1));
-		} else {
-			return (Integer) null;
-		}
-	}*/
 
-	/**
-	 * 
-	 * @param name
-	 *            the option name
-	 * @return a string matching <b>name</b>
-	 */
-	/*public String fetchStringFromOptions(String name) {
-		if (data.contains(name)) {
-			return data.get(data.indexOf(name) + 1);
-		} else {
-			return null;
-		}
-	}*/
-
-	/**
-	 * 
-	 * @param name
-	 *            the option name
-	 * @return a boolean matching <b>name</b>
-	 */
-	/*public Boolean fetchBooleanFromOptions(String name) throws Exception {
-		if (data.contains(name)) {
-			if (data.get(data.indexOf(name) + 1).equals("true")) {
-				return true;
-			} else if (data.get(data.indexOf(name) + 1).equals("false")) {
-				return false;
-			} else {
-				throw new Exception();
-			}
-		} else {
-			return null;
-		}
-	}*/
 	/**
 	 * this checks if the data list contains any number of specified strings.
 	 * This is to be used for checking existing variable names in the current
 	 * data.
+	 * 
 	 * @param v1
 	 * @param vMore
 	 * @return
@@ -129,18 +102,19 @@ public class OptionsFile {
 		try {
 			PrintWriter utdata = new PrintWriter(new BufferedWriter(
 					new FileWriter(path)));
-			utdata.println("SizeX: " + Game.getGameContainer().getScreenWidth());
-			utdata.println("SizeY: "
+			utdata.println(FIELD_RESOLUTION_X + " "
+					+ Game.getGameContainer().getScreenWidth());
+			utdata.println(FIELD_RESOLUTION_Y + " "
 					+ Game.getGameContainer().getScreenHeight());
-			utdata.println("FullScreen: " + true);
-			utdata.println("MvLeft: " + 65);
-			utdata.println("MvRight: " + 68);
-			utdata.println("Jump: " + 32);
-			utdata.println("Fire: " + 0);
-			utdata.println("Interract: " + 2);
-			utdata.println("MusicVolume: "
+			utdata.println(FIELD_FULLSCREEN + " " + true);
+			utdata.println(FIELD_KEY_MOVELEFT + " " + 65);
+			utdata.println(FIELD_KEY_MOVERIGHT + " " + 68);
+			utdata.println(FIELD_KEY_JUMP + " " + 32);
+			utdata.println(FIELD_KEY_FIRE + " " + 0);
+			utdata.println(FIELD_KEY_INTERRACT + " " + 2);
+			utdata.println(FIELD_VOLUME_MUSIC + " "
 					+ Game.getGameContainer().getMusicVolume());
-			utdata.println("SoundVolume: "
+			utdata.println(FIELD_VOLUME_SOUND + " "
 					+ Game.getGameContainer().getSoundVolume());
 			utdata.close();
 		} catch (IOException ex) {
@@ -148,27 +122,31 @@ public class OptionsFile {
 		}
 
 	}
+
 	/**
 	 * adds one value to the list of proper type.
-	 * @param list the list to add to
-	 * @param indata the Scanner
+	 * 
+	 * @param list
+	 *            the list to add to
+	 * @param indata
+	 *            the Scanner
 	 */
-	private static void addValue(ArrayList<?super Object> list, Scanner indata){
-		if(indata.hasNextBoolean()){
+	private static void addValue(ArrayList<? super Object> list, Scanner indata) {
+		if (indata.hasNextBoolean()) {
 			list.add(indata.nextBoolean());
-		}else if(indata.hasNextByte()){
+		} else if (indata.hasNextByte()) {
 			list.add(indata.nextByte());
-		}else if(indata.hasNextInt()){
+		} else if (indata.hasNextInt()) {
 			list.add(indata.nextInt());
-		}else if(indata.hasNextLong()){
+		} else if (indata.hasNextLong()) {
 			list.add(indata.nextLong());
-		}else if(indata.hasNextShort()){
+		} else if (indata.hasNextShort()) {
 			list.add(indata.nextShort());
-		}else if(indata.hasNextDouble()){
+		} else if (indata.hasNextDouble()) {
 			list.add(indata.nextDouble());
-		}else if(indata.hasNextFloat()){
+		} else if (indata.hasNextFloat()) {
 			list.add(indata.nextFloat());
-		}else if(indata.hasNext()){
+		} else if (indata.hasNext()) {
 			list.add(indata.next());
 		}
 	}
@@ -176,11 +154,10 @@ public class OptionsFile {
 	/**
 	 * loads the option values
 	 * 
-	 * @return
+	 * @return a list of values of varying types(int, double, boolean, etc.)
 	 */
-	private static ArrayList<?super Object> loadData() {
-		@SuppressWarnings({ "rawtypes", "unchecked" })
-		ArrayList<?super Object> res = new ArrayList();
+	private static ArrayList<? super Object> loadData() {
+		ArrayList<? super Object> res = new ArrayList<Object>();
 		try {
 			Scanner indata = new Scanner(new File(path));
 
